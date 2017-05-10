1e5daf65c9962d28d8cf22b002b55bc5e008555f
regireed89 <regireed89@gmail.com>
2017-05-10 17:55:05 -0500

    spawn objects

---
+++ Assets/Scripts/MakeObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeObjects: MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+
+}
