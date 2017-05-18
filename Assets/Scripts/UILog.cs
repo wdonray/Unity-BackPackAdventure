7cfe85faa1393fa128238377762f7df16de3ca65
regireed89 <regireed89@gmail.com>
2017-05-18 09:37:02 -0500

    text

---
+++ Assets/Scripts/UILog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILog : MonoBehaviour
+{
+
+    public Text text;
+    public float time = 2.0f;
+    private void Start()
+    {
+        text.text = "";
+    }
+
+    private void Update()
+    {
+        if (time < 0)
+        {
+            text.text = " ";
+            time = 2.0f;
+        }
+        time -= Time.deltaTime;
+    }
+
+
+    public void OnItemAction(ScriptableObjects.Item item)
+    {
+        text.text = item.name + "\n";
+
+    }
+
+}
