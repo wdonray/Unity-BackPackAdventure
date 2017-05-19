46d51b68d2535132f08ce8fc20aad8b4f1f9eb05
Donray Williams <w.donray@yahoo.com>
2017-05-19 18:15:50 -0500

    awwww

--- Assets/Scripts/BackPack.cs
+++ Assets/Scripts/BackPack.cs
@@ -1,60 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using ScriptableObjects;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Assertions;
+#if UNITY_EDITOR
+using UnityEditor;
 
+#endif
 public class BackPack : MonoBehaviour
 {
+    public string FileNameToSave;
     private int _idcount;
-
-    //private CombatKnife _knife; // Used for testing 
 
     public OnBackPackChange BackPackChange = new OnBackPackChange();
     public BackPackConfig BackPackConfig; // Referencing the BackPackConfig
     public List<Item> Inventory; // List to add items to
     public int ListCapicty; // Variable to set equal to the capacity in the config 
+    public List<Item> __Inventory = new List<Item>();
+    private BackPackConfig currentBackPack;
 
     // Use this for initialization
     private void Start()
     {
         _idcount = 0;
         Inventory = new List<Item>();
-        //_knife = new CombatKnife();
-        ListCapicty = BackPackConfig.Capacity;
-        foreach (var i in BackPackConfig.Items)
-        {
-            var startItems = Instantiate(i);
-            AddToStash(startItems);
-        }
+        currentBackPack = ScriptableObject.CreateInstance<BackPackConfig>();
+        if (BackPackConfig == null)
+            BackPackConfig = LoadBackPack(FileNameToSave);
+        Assert.IsNotNull(BackPackConfig);
+        Assert.IsNotNull(BackPackConfig.Items, "no items");
+        BackPackConfig.Items.ForEach(AddToStash);
     }
-
-    // Testing: Working
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //        AddToStash(_knife);
-    //    else if (Input.GetKeyDown(KeyCode.Delete))
-    //        RemoveFromStash(_knife);
-    //}
-
     /// Adding in any item taken in to the list if not over Capicity
     public void AddToStash(Item item)
     {
-        if (Inventory.Count >= ListCapicty) return;
         Inventory.Add(item);
-        BackPackChange.Invoke(this);
-        AddInfo(item);
+        BackPackChange.Invoke(currentBackPack);
+        currentBackPack.InitInventory = Inventory;
     }
 
-    // Add item at given index
-    //public void AddToStashAtIndex(Item item, int choosenIndexAt)
-    //{
-    //    if (Inventory.Count >= ListCapicty) return;
-    //    if (choosenIndexAt > ListCapicty || choosenIndexAt < 0) return;
-    //    Inventory.Insert(choosenIndexAt, item);
-    //    AddInfo(item);
-    //}
     /// If the list contains the item remove it from the list
     public void RemoveFromStash(Item item)
     {
@@ -62,8 +49,9 @@ public class BackPack : MonoBehaviour
         if (Inventory == null) return;
         if (!Inventory.Contains(item)) return;
         Inventory.Remove(item);
-        BackPackChange.Invoke(this);
+        BackPackChange.Invoke(currentBackPack);
         Debug.Log("Removed: " + item.GetType() + " " + _idcount);
+        currentBackPack.InitInventory = Inventory;
     }
 
     [ContextMenu("Delete List")]
@@ -71,24 +59,15 @@ public class BackPack : MonoBehaviour
     public void RemoveAllFromStash()
     {
         if (Inventory == null) return;
-        BackPackChange.Invoke(this);
+        BackPackChange.Invoke(currentBackPack);
         Inventory = new List<Item>();
         foreach (var i in BackPackConfig.Items)
         {
             var startItems = Instantiate(i);
             AddToStash(startItems);
         }
+        currentBackPack.InitInventory = Inventory;
     }
-
-    // Used in more then once place
-    private void AddInfo(Item item)
-    {
-        _idcount++;
-        item.Identification = _idcount;
-        item.Name = gameObject.name + _idcount;
-        Debug.Log("Added: " + item + " " + _idcount);
-    }
-
     // Raise the list capacity
     public void RaiseCapacity(int amount)
     {
@@ -103,8 +82,55 @@ public class BackPack : MonoBehaviour
         Debug.Log("Lowered Capacity by:" + amount);
     }
 
+    public void SaveBackPack(string filename, BackPackConfig backPack)
+    {
+        var json = JsonUtility.ToJson(currentBackPack, true);
+        var path = Application.persistentDataPath + string.Format("/{0}.json", filename);
+        File.WriteAllText(path, json);
+    }
+
+    public void Save()
+    {
+        SaveBackPack(FileNameToSave, currentBackPack);
+        BackPackChange.Invoke(currentBackPack);
+    }
+
+    public void Load()
+    {
+        currentBackPack = LoadBackPack(FileNameToSave);
+        BackPackChange.Invoke(currentBackPack);
+        currentBackPack.InitInventory = Inventory;
+    }
+    public BackPackConfig LoadBackPack(string filename)
+    {
+        var backpack = ScriptableObject.CreateInstance<BackPackConfig>();
+        var path = Application.persistentDataPath + string.Format("/{0}.json", filename);
+        var json = File.ReadAllText(path);
+        #region Editor
+#if UNITY_EDITOR
+        var backPackAsset = AssetDatabase.FindAssets("t:BackPackConfig")
+            .Select(AssetDatabase.AssetPathToGUID)
+            .Select(AssetDatabase.LoadAssetAtPath<BackPackConfig>)
+            .Where(b => b)
+            .ToArray();
+        backpack = backPackAsset.FirstOrDefault();
+#endif
+        #endregion
+        if (backpack == null)
+            backpack = ScriptableObject.CreateInstance<BackPackConfig>();
+        Assert.IsNotNull(backpack, "BackPack should not be null");
+        var asset = Resources.FindObjectsOfTypeAll<BackPackConfig>().FirstOrDefault();
+        JsonUtility.FromJsonOverwrite(json, asset);
+        backpack = asset;
+        foreach (var b in backpack.Items)
+        {
+            __Inventory.Add(b);
+        }
+        return backpack;
+    }
+
     [Serializable]
-    public class OnBackPackChange : UnityEvent<BackPack>
+    public class OnBackPackChange : UnityEvent<BackPackConfig>
     {
     }
 }
